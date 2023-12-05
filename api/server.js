896eb4409b96965aa515ab044b28f708194b8642
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-05 14:31:20 -0600

    complete

--- api/server.js
+++ api/server.js
@@ -1,12 +1,5 @@
 const express = require('express')
 const User = require('./users/model')
-/* User =
-  find()
-  findById(id)
-  insert({ name, bio })
-  update(id, changes)
-  remove(id)
-*/
 const server = express()
 server.use(express.json())
 
@@ -14,7 +7,7 @@ server.use(express.json())
 server.post('/api/users', (req, res) => {
   if (!req.body.name || !req.body.bio) {
     res.status(400).json({
-      message: "Please provide name and bio for the user"
+      message: "Please provide name and bio for the user",
     })
   } 
   else {
@@ -38,7 +31,7 @@ server.get('/api/users/:id', (req, res) => {
     .then(user =>{
       if (!user) {
         res.status(404).json({
-          message: "The user with the specified ID does not exist" 
+          message: "The user with the specified ID does not exist",
         })
       }
       else res.json(user)
@@ -91,37 +84,36 @@ server.delete('/api/users/:id', async (req, res) => {
   }
 })
 
-// PUT a user (and get back the modified user)
-/*
-- If the _user_ with the specified `id` is not found:
-
-  - respond with HTTP status code `404` (Not Found).
-  - return the following JSON object: `{ message: "The user with the specified ID does not exist" }`.
-
-- If the request body is missing the `name` or `bio` property:
-
-  - respond with HTTP status code `400` (Bad Request).
-  - return the following JSON response: `{ message: "Please provide name and bio for the user" }`.
-
-- If there's an error when updating the _user_:
-
-  - respond with HTTP status code `500`.
-  - return the following JSON object: `{ message: "The user information could not be modified" }`.
-
-- If the user is found and the new information is valid:
-
-  - update the user document in the database using the new information sent in the `request body`.
-  - respond with HTTP status code `200` (OK).
-  - return the newly updated _user document_.
-*/
-server.put('/api/users/:id', (req, res) => {
-  
+// PUT (modify) a user
+server.put('/api/users/:id', async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if (!req.body.name || !req.body.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    })
+  }
+  else if (!user) {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist",
+    })
+  }
+  else {
+    User.update(user.id, req.body)
+      .then(updatedUser => res.status(200).json(updatedUser))
+      .catch(err => {
+        res.status(500).json({
+          message: "The user information could not be modified",
+          err: err.message,
+          stack: err.stack,
+        })
+      })
+  }
 })
 
 // USE when all else fails
 server.use('*', (req, res) => {
   res.status(404).json({
-    message: 'request not found'
+    message: 'request not found',
   })
 })
 
