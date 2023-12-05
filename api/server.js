9fd4deb0153a474cf583733fdd6b892bc26edbf8
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-05 13:45:31 -0600

    post request complete

--- api/server.js
+++ api/server.js
@@ -8,26 +8,28 @@ const User = require('./users/model')
   remove(id)
 */
 const server = express()
+server.use(express.json())
 
 // POST a user
-/*
-- If the request body is missing the `name` or `bio` property:
-
-  - respond with HTTP status code `400` (Bad Request).
-  - return the following JSON response: `{ message: "Please provide name and bio for the user" }`.
-
-- If the information about the _user_ is valid:
-
-  - save the new _user_ the the database.
-  - respond with HTTP status code `201` (Created).
-  - return the newly created _user document_ including its id.
-
-- If there's an error while saving the _user_:
-  - respond with HTTP status code `500` (Server Error).
-  - return the following JSON object: `{ message: "There was an error while saving the user to the database" }`.
-*/
 server.post('/api/users', (req, res) => {
-
+  if (!req.body.name || !req.body.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user"
+    })
+  } 
+  else {
+    User.insert(req.body)
+      .then(user =>{
+        res.status(201).json(user)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error while saving the user to the database",
+          err: err.message,
+          stack: err.stack,
+        })
+      })
+  }
 })
 
 // GET a user
